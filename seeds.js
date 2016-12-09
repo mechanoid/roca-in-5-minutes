d8b3ef1f95ddbb2dc464501a66b9416f871d07d5
Falk Hoppe <falkhoppe81@gmail.com>
2016-12-09 15:47:34 +0100

    add basic store management with example routes

---
+++ seeds.js
@@ -0,0 +1,7 @@
+module.exports = ({
+  // ... put your initial state in here
+  creatures: [
+    { name: 'Unicorn', power: 'Stardust' },
+    { name: 'Troll', power: 'Treepunch' }
+  ]
+})
---
+++ server.js
@@ -0,0 +1,25 @@
+const setup = require('./lib/setup')
+const seeds = require('./seeds')
+const store = require('./lib/store')(seeds)
+
+const app = setup(store, { port: 3000 })
+
+// ***********************************************************************
+// Default View Rendering
+// ***********************************************************************
+
+// all views below views are registered as GETable views,
+// so the example pages `creatures.pug` and `creatures/new.pug`
+// are browsable by calling /creatures or /creatures/new
+
+// ***********************************************************************
+// Add your own express routes below
+//
+// add post, put or delete routes to manipulate the store,
+// as shown in the example route for creatures.
+// ***********************************************************************
+
+app.post('/creatures', (req, res) => {
+  store.creatures.push({ name: req.body.name, power: req.body.power })
+  res.redirect('/creatures')
+})
