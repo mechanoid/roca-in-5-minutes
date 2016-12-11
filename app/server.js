4e1bb847a566ec0a0f1642eedbd6e867d8f9ded1
Falk Hoppe <falkhoppe81@gmail.com>
2016-12-11 20:45:18 +0100

    add edit route and restructure application setup

--- app/server.js
+++ app/server.js
@@ -1,8 +1,8 @@
-const setup = require('../lib/setup')
+const application = require('../lib/application')
 const seeds = require('./seeds')
 const store = require('../lib/store')(seeds)
 
-const app = setup(store, { port: 3000 })
+const app = application.bootstap()
 
 // ***********************************************************************
 // Default View Rendering
@@ -19,7 +19,43 @@ const app = setup(store, { port: 3000 })
 // as shown in the example route for creatures.
 // ***********************************************************************
 
+const hasSameName = (instance, name) =>
+  instance.name.toLocaleLowerCase() === name.toLocaleLowerCase()
+
 app.post('/creatures', (req, res) => {
   store.creatures.push({ name: req.body.name, power: req.body.power })
   res.redirect('/creatures')
 })
+
+app.get('/creatures/:name', (req, res) => {
+  const creature = store.creatures.find(c => hasSameName(c, req.params.name))
+
+  if (creature) {
+    return res.render('creatures/edit', { creature })
+  }
+
+  res.render('404')
+})
+
+app.post('/creatures/:name', (req, res) => {
+  const index = store.creatures.findIndex(c => hasSameName(c, req.params.name))
+
+  if (index >= 0) {
+    const { name, power } = req.body
+
+    const newCreature = Object.assign({}, store.creatures[index], { name, power })
+
+    store.creatures[index] = newCreature
+
+    return res.redirect('/creatures')
+  }
+
+  res.render('400')
+})
+
+// ***********************************************************************
+// Basic application setup, which binds the dynamic view routes and tries
+// to start the server on the provided default port.
+// ***********************************************************************
+
+application.run(app, store, { port: 3000 })
